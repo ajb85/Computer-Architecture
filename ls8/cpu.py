36c43e34793ea372d89b94dac78daa89517f3111
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-07-24 18:07:54 -0400

    Finishes Day 3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,32 +9,84 @@ call = 0b01010000
 add = 0b10100000
 ret = 0b00010001
 mul = 0b10100010
+push = 0b01000101
+pop = 0b01000110
+
+class RAM:
+    def __init__(self, size):
+        self.storage = [None] * size
+
+    def pop(self, pointer):
+        if(pointer >= 0 and pointer < len(self.storage)):
+            value = self.storage[pointer]
+            self.storage[pointer] = None
+            return value
+        return None
+
+    def push(self, pointer, value):
+        if(pointer >= 0 and pointer < len(self.storage)):
+            if(self.storage[pointer] == None):
+                self.storage[pointer] = value
+                return True
+            print(f"Possible Stack Overflow found at {pointer}: {self.storage[pointer]}")
+        return False
+
+    def read(self, address):
+        return self.storage[address] if address < len(self.storage) else None
+
+    def write(self, address, value):
+        self.storage[address] = value
+
+    def getSize(self):
+        return len(self.storage)
 
 class Commands:
     def __init__(self, cpu):
-        self.list = {ldi: self.ldi, prn:self.prn, hlt:self.hlt, mul:self.mul}
-        self.list[ldi] = self.ldi
+        self.list = {
+            ldi: self.ldi, 
+            prn:self.prn, 
+            hlt:self.hlt, 
+            mul:self.mul, 
+            push:self.push, 
+            pop:self.pop
+        }
         self.cpu = cpu
 
     def ldi(self):
-        op_a = self.cpu.ram_read(self.cpu.pc + 1)
-        op_b = self.cpu.ram_read(self.cpu.pc + 2)
+        op_a = self.cpu.ram.read(self.cpu.pc + 1)
+        op_b = self.cpu.ram.read(self.cpu.pc + 2)
         self.cpu.register[op_a] = op_b
         return 2
 
     def prn(self):
-        op_a = self.cpu.ram_read(self.cpu.pc + 1)
+        op_a = self.cpu.ram.read(self.cpu.pc + 1)
         print(self.cpu.register[op_a])
         return 1
-
+        
     def mul(self):
-        op_a = self.cpu.ram_read(self.cpu.pc + 1)
-        op_b = self.cpu.ram_read(self.cpu.pc + 2)
-        print(self.cpu.register[op_a] * self.cpu.register[op_b])
+        op_a = self.cpu.ram.read(self.cpu.pc + 1)
+        op_b = self.cpu.ram.read(self.cpu.pc + 2)
+        print(self.cpu.ram.read(op_a) * self.cpu.ram.read(op_b))
         return 2
 
+    def push(self):
+        op_a = self.cpu.ram.read(self.cpu.pc + 1)
+        pointer = self.cpu.register[len(self.cpu.register) - 1]
+        wasSuccessful = self.cpu.ram.push(pointer - 1, self.cpu.register[op_a])
+        if(wasSuccessful):
+            self.cpu.register[len(self.cpu.register) - 1] = pointer - 1
+        return 1
+
+    def pop(self):
+        op_a = self.cpu.ram.read(self.cpu.pc + 1)
+        pointer = self.cpu.register[len(self.cpu.register) - 1]
+        value = self.cpu.ram.pop(pointer)
+        self.cpu.register[op_a] = value
+        self.cpu.register[len(self.cpu.register) - 1] = pointer + 1
+        return 1
+
     def hlt(self):
-        return len(self.cpu.ram)
+        return self.cpu.ram.getSize()
 
 class CPU:
     """Main CPU class."""
@@ -42,29 +94,21 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.register = [0] * 8
-        self.ram = [None] * 256
+        self.ram = RAM(256)
+        self.register[len(self.register) - 1] = self.ram.getSize() - 1
         self.pc = 0
         self.commands = Commands(self)
 
     def load(self):
         """Load a program into memory."""
-        # For now, we've just hardcoded a program:
         with open(f"{sys.argv[1]}") as f:
+            # Load user-specified file and save it in RAM
             i = 0
             for line in f:
-                if(i < len(self.ram)):
-                    command = line.replace(" ", "")[:8]
-                    if(self.isValidCommand(command)):
-                        self.ram[i] = int(command, 2)
-                    i += 1
-                else:
-                    print("STACK OVERFLOW") 
-                
-    def ram_read(self, address):
-        return self.ram[address] if address < len(self.ram) else None
-
-    def ram_write(self, address, value):
-        self.ram[address] = value
+                command = line.replace(" ", "")[:8]
+                if(self.isValidCommand(command)):
+                    self.ram.write(i, int(command, 2))
+                i += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -85,9 +129,9 @@ class CPU:
             self.pc,
             #self.fl,
             #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram.read(self.pc),
+            self.ram.read(self.pc + 1),
+            self.ram.read(self.pc + 2)
         ), end='')
 
         for i in range(8):
@@ -96,7 +140,7 @@ class CPU:
         print()
 
     def run(self):
-        ir = self.ram_read(0)
+        ir = self.ram.read(0)
         while True:
             if(ir == None):
                 return
@@ -107,7 +151,7 @@ class CPU:
                 print(f"ERROR: Invalid command {ir}")
             
             self.pc += 1 + inc
-            ir = self.ram_read(self.pc)
+            ir = self.ram.read(self.pc)
     
     def isValidCommand(self, command):
         for char in command:
